d1cb0a0291eb150f5cc73083236cb39f7ac35832
CodeSandbox <support@codesandbox.io>
2025-08-26 13:17:15 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-var http = require("http");
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const dev = process.env.NODE_ENV !== "production";
+
+const server = express();
+const httpServer = http.createServer(server);
+const io = socketIo(httpServer);
+
+server.get("/", (req, res) => {
+  res.send("This is form server");
+});
+
+// Socket.IO connection
+io.on("connection", (socket) => {
+  console.log("New client connected", socket.id);
+  socket.on("connect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${PORT}`);
+});
