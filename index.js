2e14c3283e181cbb9a73c0770fd43488dfd0d6a7
Ravi Yadav <88570593+RaviYadav369@users.noreply.github.com>
2025-08-27 09:15:49 +0000

    update the cors

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const socketIo = require("socket.io");
 const dev = process.env.NODE_ENV !== "production";
 
 const server = express();
-const httpServer = http.createServer(server);
+const httpServer = http.createServer(server, {
+  cors: {
+    origin: "*",
+    method: ["GET", "POST"],
+  },
+});
 const io = socketIo(httpServer);
 
 server.get("/", (req, res) => {
